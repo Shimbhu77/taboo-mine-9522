a572c16e1bd077bda11690e188d635ed1f66f4dc
Namdev Patil <namdevpatilm@gmail.com>
2022-07-20 16:18:20 +0530

    bestseller added

---
+++ navbar/script/footer4hp.js
@@ -0,0 +1,51 @@
+function navbar_h(){
+    return ` <div class="head1_A">
+    <div class="promocode_A">
+        <p>
+            2 days only!
+            <span> $5.50 SELECT MEN'S BODY CARE </span>
+            <span>Code: SUPERSALE</span>
+            <a href="https://www.bathandbodyworks.com/details-popup-ee-supersale.html">Details</a>
+        </p>
+    </div>
+    </div>
+    <div class="head2_A">
+    <div>
+        <img src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/on/demandware.static/Sites-BathAndBodyWorks-Site/-/en_US/v1655286872912/images/svg-icons/bopis-icon-small.svg?yocs=o_s_"
+            alt="">
+        <span>Pick Up In Store</span>
+        <a
+            href="https://www.bathandbodyworks.com/on/demandware.store/Sites-BathAndBodyWorks-Site/en_US/Stores-BopisStoreFinder">Set
+            Store</a>
+    </div>
+    
+    </div>
+    <div class="head3_A">
+    <div class="logo_A">
+        <img src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/on/demandware.static/Sites-BathAndBodyWorks-Site/-/default/dwa9385fe9/images/svg-icons/Logos-main.svg?yocs=o_s_"
+            alt="">
+    
+        <div>
+            <fieldset>
+                <input type="text" id="input_a" placeholder="Search by fragrance or product...">
+                <button id="search_A"><img src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/on/demandware.static/Sites-BathAndBodyWorks-Site/-/default/dwbfdc63c5/images/svg-icons/UI-Search-v2.svg?yocs=o_s_" alt=""></button>
+            </fieldset>
+            <a href="./signIn_and_signUp/signIn.html"><img src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/on/demandware.static/Sites-BathAndBodyWorks-Site/-/default/dw3e39cc25/images/svg-icons/UI-MyAccount.svg?yocs=o_s_" alt=""></a>
+            <a href="./Cart/cartpage.html"><img src="https://www.bathandbodyworks.in/on/demandware.static/Sites-BathAndBodyWorks-Site/-/default/dw8581bc7e/images/AddToBag.png" alt=""></a>
+        </div>
+    
+    </div>
+    <div class="category_A">
+        <a href="./All Products/allProducts.html" id="nav1_h">ALL PRODUCTS</a>
+        <a href="./chacha/New And Now.html" id=nav2_h>NEW & NOW</a>
+        <a href="./BodyCare/bodycare.html" id=nav3_h>BODY CARE</a>
+        <a href="" id=nav4_h>CANDLES</a>
+        <a href="./Sugandh/sugandh.html" id=nav5_h>FRAGRANCE</a>
+        <a href="" id=nav6_h>HAND SHOP'S & SANITIZERS</a>
+        <a href="" id=nav7_h>MEN'S</a>
+        <a href="./gifts/index.html" id=nav8_h>GIFTS</a>
+    </div>
+    </div>`
+    }
+    
+    export {navbar_h} ;
