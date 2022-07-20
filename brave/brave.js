a57a10dbd67e9aed3112513d1ad31222dab8dfb3
akshay <akshaysuman129@gmail.com>
2022-07-20 21:37:45 +0530

    added

---
+++ brave/brave.js
@@ -0,0 +1,311 @@
+let producArr=[
+    {
+        id:1,
+        image:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/6_1_700x.jpg?v=1652855125",
+        image2:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/Spot-clear-pimple-gel_1_533x.jpg?v=1653933473",
+        title:"brave essential-2 in 1 shampoo &styling combo",
+        content:"Nourishes & Stimulates Growth",
+        discount:"30%",
+        rating:4,
+        price:345,
+        strikeprice:299,
+        icon:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcsOLVQHyeAAo14YkdU-JOBDLzf9FdTDH_vw&usqp=CAU",
+        
+    },
+    {
+        id:2,
+        image:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/Artboard1_20_533x.jpg?v=1655711584",
+        image2:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/DetanFacewash_1_700x.jpg?v=1653933020",
+        title:"brave essentials - complete body essential combo",
+        content:"aqua noir body wash,body scrub",
+        discount:"30%",
+        rating:4,
+        price:"633",
+        strikeprice:299,
+        icon:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcsOLVQHyeAAo14YkdU-JOBDLzf9FdTDH_vw&usqp=CAU",
+    },
+    {
+        id:3,
+        image:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/Artboard1_1_8352d583-8302-4911-b42a-d6901acf611f_533x.jpg?v=1652855227",
+        image2:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/Exfoliating-body-wash_1_533x.jpg?v=1653933850",
+        title:"brave essentials-hair oil &massage oil",
+        content:"hair oil |soft massage & anti-bacterial",
+        discount:"30%",
+        rating:4,
+        price:345,
+        strikeprice:299,
+        icon:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcsOLVQHyeAAo14YkdU-JOBDLzf9FdTDH_vw&usqp=CAU",
+    },
+    {
+        id:4,
+        image:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/2_2_700x.jpg?v=1652853851",
+        image2:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/Turmeric-body-wash_1_533x.jpg?v=1653933684",
+        title:"brave essentials-everyday skincare combo",
+        content:"detan face wash,sunscreen spf 50",
+        discount:"30%",
+        rating:4,
+        price:499,
+        strikeprice:698,
+        icon:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcsOLVQHyeAAo14YkdU-JOBDLzf9FdTDH_vw&usqp=CAU",
+    },
+    {    id:5,
+        image:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/Artboard1_a654a940-088c-4b09-9f15-dbfb4600513f_533x.jpg?v=1653049227",
+        image2:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/Daily-beard-wash_1_533x.jpg?v=1653932743",
+        title:"beardo facewash",
+        content:"mild facewash",
+        discount:"30%",
+        rating:4,
+        price:345,
+        strikeprice:299,
+        icon:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcsOLVQHyeAAo14YkdU-JOBDLzf9FdTDH_vw&usqp=CAU",
+    },
+    {
+        id:6,
+        image:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/Artboard1_1_a0b78020-d351-4908-9aa5-e9a6976cb552_533x.jpg?v=1652861079",
+        image2:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/LipBalmListing_1_533x.jpg?v=1653933424",
+        title:"beardo facewash",
+        content:"mild facewash",
+        discount:"30%",
+        rating:4,
+        price:345,
+        strikeprice:299,
+        icon:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcsOLVQHyeAAo14YkdU-JOBDLzf9FdTDH_vw&usqp=CAU",
+    },
+    {
+        id:6,
+        image:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/1_5_700x.jpg?v=1654254927",
+        image2:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/Impression-parfum_700x.jpg?v=1656411151",
+        title:"brave essentials impression perfume, 100ml",
+        content:"a luxurious & long-lasting fragnance",
+        discount:"25%",
+        rating:5,
+        price:449,
+        strikeprice:599,
+        icon:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcsOLVQHyeAAo14YkdU-JOBDLzf9FdTDH_vw&usqp=CAU",
+    },
+    {
+        id:7,
+        image:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/Artboard1_7_700x.jpg?v=1652458103",
+        image2:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/Charcoal-body-scrub_1_700x.jpg?v=1653932920",
+        title:"brave essentials charcoal body scrub, 100gm",
+        content:"remove dirt & tan exfoliates dead skin cells",
+        discount:"25%",
+        rating:5,
+        price:209,
+        strikeprice:299,
+        icon:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcsOLVQHyeAAo14YkdU-JOBDLzf9FdTDH_vw&usqp=CAU",
+    },
+    {
+        id:8,
+        image:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/Artboard1_10_700x.jpg?v=1652460567",
+        image2:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/LipBalmListing_1_700x.jpg?v=1653933424",
+        title:"brave essentials- roll on deodorant ,50ml",
+        content:"anti perspirant | lighten under arams",
+        discount:"25%",
+        rating:4.7,
+        price:239,
+        strikeprice:299,
+        icon:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcsOLVQHyeAAo14YkdU-JOBDLzf9FdTDH_vw&usqp=CAU",
+    },
+    {
+        id:9,
+        image:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/1_2_c18c3527-cf20-4ace-a9f4-a88671256bb1_700x.jpg?v=1654065362",
+        image2:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/3_1_700x.jpg?v=1654065362",
+        title:"brave essentials -all body washes combo, 200ml each",
+        content:"body washes combo",
+        discount:"40%",
+        rating:5,
+        price:838,
+        strikeprice:1396,
+        icon:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcsOLVQHyeAAo14YkdU-JOBDLzf9FdTDH_vw&usqp=CAU",
+    },
+    {
+        id:10,
+        image:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/image_700x.jpg?v=1655122401",
+        image2:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/Nourishing-hair-cream_1_700x.jpg?v=1655122401",
+        title:"brave essentials nourishing hair cream, 100gm",
+        content:"non-greasy | soft hold styling",
+        discount:"30%",
+        rating:5,
+        price:209,
+        strikeprice:299,
+        icon:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcsOLVQHyeAAo14YkdU-JOBDLzf9FdTDH_vw&usqp=CAU",
+    },
+];
+
+append(producArr);
+
+function append(data){
+    data.forEach(element => {
+        let div=document.createElement("div");
+        //let discount=dcoument.createElement("p");
+       // discount.innerText=element.discount;
+        let img1=document.createElement("img");
+        img1.src=element.image;
+        img1.setAttribute("class","back_img");
+        let img2=document.createElement("img");
+        img2.src=element.image2;
+        img2.setAttribute("class","front_img");
+
+        let p1=document.createElement("p");
+        p1.innerText=element.title;
+
+        let p2=document.createElement("span");
+        p2.innerText=element.discount;
+        p2.setAttribute("class","discount");
+
+
+        let box_1=document.createElement("div");
+        box_1.setAttribute("class","box_1");
+
+        let first_box=document.createElement("div");
+       
+        let li1=document.createElement("span");
+        li1.innerText=element.price;
+        let li2=document.createElement("span");
+        li2.innerText=element.strikeprice;
+
+        let bt1=document.createElement("button");
+        bt1.innerText="add to cart";
+        let p3=document.createElement("span");
+        p3.innerText=element.content;
+        p3.setAttribute("class","content");
+
+       
+        first_box.append(li1,li2);
+
+        let second_box=document.createElement("div");
+
+       
+        let li3=document.createElement("span");
+        li3.innerText=element.rating;
+        let img_icon=document.createElement("img");
+        img_icon.src=element.icon;
+
+       
+        second_box.append(li3,img_icon);
+
+        box_1.append(first_box,second_box);
+        div.append(img1,img2,p1,p3,p2,box_1,bt1);
+        document.querySelector("#container").append(div);
+        
+    });
+}
+
+
+
+
+
+let bestArr=[
+{
+        id:1,
+        image:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/Artboard1_1_335c1545-0fc8-4689-949d-51d77f268530_533x.jpg?v=1652861341",
+        title:"brave essentials - de-tan face scrub, 75ml",
+        content:"brightens | removes dead skin & blackheads",
+        discount:"30%",
+        rating:5,
+        price:228,
+        strikeprice:325,
+        icon:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcsOLVQHyeAAo14YkdU-JOBDLzf9FdTDH_vw&usqp=CAU",
+        
+    },
+    {
+        id:2,
+        image:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/Artboard1_1_a0b78020-d351-4908-9aa5-e9a6976cb552_533x.jpg?v=1652861079",
+        title:"brave essentials - beard grooming combo",
+        content:"beard wash,beard oil,& moustache wax",
+        discount:"30%",
+        rating:4.7,
+        price:614,
+        strikeprice:877,
+        icon:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcsOLVQHyeAAo14YkdU-JOBDLzf9FdTDH_vw&usqp=CAU",
+        
+    },
+    {
+        id:3,
+        image:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/shampoo_1_f4b738d9-6056-4fb9-98e5-618387369ec9_533x.jpg?v=1652856621",
+        title:"brave essentials -top to toe combo",
+        content:"2 in 1 shampoo conditioner turmeric condtioner",
+        discount:"30%",
+        rating:4.6,
+        price:733,
+        strikeprice:1047,
+        icon:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcsOLVQHyeAAo14YkdU-JOBDLzf9FdTDH_vw&usqp=CAU",
+        
+    },
+    {
+        id:4,
+        image:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/Artboard1_1_8352d583-8302-4911-b42a-d6901acf611f_533x.jpg?v=1652855227",
+        title:"brave essentials -Hair stylimg combo",
+        content:"hair oil,2-in-1 shampoo wax,massage oil",
+        discount:"25%",
+        rating:5,
+        price:751,
+        strikeprice:1073,
+        icon:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcsOLVQHyeAAo14YkdU-JOBDLzf9FdTDH_vw&usqp=CAU",
+        
+    },
+
+    
+];
+
+appendBest(bestArr);
+
+function appendBest(data){
+data.forEach(element => {
+    let div=document.createElement("div");
+
+    let pic_container=document.createElement("div");
+    let pic=document.createElement("img");
+      pic.src=element.image;
+      pic_container.append(pic);
+
+    let p1=document.createElement("p");
+        p1.innerText=element.title;
+
+        let p2=document.createElement("span");
+        p2.innerText=element.discount;
+        p2.setAttribute("class","discount");
+
+        let p4=document.createElement("span");
+        p4.innerText="#1 best seller"
+        p4.setAttribute("class","bestseller");
+
+
+
+        let box_1=document.createElement("div");
+        box_1.setAttribute("class","box_1");
+
+        let first_box=document.createElement("div");
+       
+        let li1=document.createElement("span");
+        li1.innerText=element.price;
+        let li2=document.createElement("span");
+        li2.innerText=element.strikeprice;
+
+        let bt1=document.createElement("button");
+        bt1.innerText="add to cart";
+        let p3=document.createElement("span");
+        p3.innerText=element.content;
+        p3.setAttribute("class","content");
+
+       
+        first_box.append(li1,li2);
+
+        let second_box=document.createElement("div");
+
+       
+        let li3=document.createElement("span");
+        li3.innerText=element.rating;
+        let img_icon=document.createElement("img");
+        img_icon.src=element.icon;
+
+       
+        second_box.append(li3,img_icon);
+
+        box_1.append(first_box,second_box);
+        div.append(pic_container,p1,p3,p2,p4,box_1,bt1);
+        document.querySelector(".seller_section").append(div);
+
+});
+}
