7dcdea50f5ec789345e1ee0f147836c8a9f25f30
Namdev Patil <namdevpatilm@gmail.com>
2022-07-23 20:31:42 +0530

    some changes

--- Body Care/Body Care.js
+++ Body Care/Body Care.js
@@ -108,6 +108,7 @@ function AddtoCart(elem) {
         AddCart.push(elem);
         localStorage.setItem("CartProducts", JSON.stringify(AddCart));
         console.log(elem, AddCart);
+        document.getElementById("count2").innerText = AddCart.length
         alert("Product Added to Cart Successfully");
     }
     else {
@@ -122,3 +123,4 @@ function verifyProduct(elem) {
     }
     return true;
 }
+document.getElementById("count2").innerText = AddCart.length
--- Hair Care/Hair Care.js
+++ Hair Care/Hair Care.js
@@ -111,6 +111,7 @@ function AddtoCart(elem) {
         AddCart.push(elem);
         localStorage.setItem("CartProducts", JSON.stringify(AddCart));
         console.log(elem, AddCart);
+        document.getElementById("count2").innerText = AddCart.length
         alert("Product Added to Cart Successfully");
     }
     else {
@@ -125,3 +126,4 @@ function verifyProduct(elem) {
     }
     return true;
 }
+document.getElementById("count2").innerText = AddCart.length
